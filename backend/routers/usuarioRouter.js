b3069d67573eeee0a08467862809d2afb4540671
DarwinGmez <firemarlon2311@gmail.com>
2025-04-07 18:21:21 -0500

    async agregado

--- backend/routers/usuarioRouter.js
+++ backend/routers/usuarioRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controllers/userControllers");
-
-router.post("/login", usuarioController.login);
+const authorization = require("../mildeware/authorizathion");
+router.post("/login", usuarioController.login, authorization);
 router.post("/registrar", usuarioController.createUsuario);
 
 module.exports = router;
